cdd4dca94d7271b82e9ca37af028b61cb849d95d
Stepan <yurtsiv.stepan@gmail.com>
2021-01-29 15:06:45 +0100

    start transmission daemon from main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
+import os
 from ui.index import run
 from transmission_rpc import Client
 
+def start():
+    print('Connecting to Transmission daemon')
+    client = Client(host='localhost', port=9091, username='transmission', password='password')
+    print('Connected. Starting the UI')
+    run(client)
+
 if __name__ == '__main__':
     try:
-        print('Connecting to Transmission daemon')
-        client = Client(host='localhost', port=9091, username='transmission', password='password')
-        print('Connected. Starting the UI')
-        run(client)
+        print('Starting Transmission daemon')
+        os.system('START /B "" bin/transmission/transmission-daemon --foreground --port 9091 --logfile logs.txt &')
+        start()
     except Exception as e:
         print(e)
